b26de808ce1f0ff375f9288c386bbd8fa418b941
marceloprr <marceloprr@gmail.com>
2021-05-19 22:05:14 -0300

    layout adstents

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Image from 'next/image'
-import { getChannelStatistics, getLastestVideos } from '../lib/youtubeChannel'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
+import { getChannelStatistics, getLastestVideos } from "../lib/youtubeChannel";
+import Date from "../components/date";
 
 export async function getStaticProps() {
-  const channelStatistics = await getChannelStatistics()
-  const lastestVideos = await getLastestVideos()
+  const channelStatistics = await getChannelStatistics();
+  const lastestVideos = await getLastestVideos();
   return {
     props: {
       channelStatistics,
-      lastestVideos
+      lastestVideos,
     },
-    revalidate: 10
-  }
+    revalidate: 10,
+  };
 }
 
 export default function Home({ channelStatistics, lastestVideos }) {
@@ -23,46 +23,62 @@ export default function Home({ channelStatistics, lastestVideos }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMdCenter}><br />
-        <h1 className={utilStyles.heading2Xl}>E aí Galerinha, tudo beleza?!?!<br />Sejam todos bem vindos ao Canal DPRC!</h1><br />
-        <p>Esse é o site sobre canal DPRC no{' '}<a href="/redir/mainchannel" target="_blank">Youtube{' '}
-          <Image
-            src="/images/yt_icon_rgb.png"
-            height={20}
-            width={26} />
-        </a> protagonizado por Christian e MarceloP!!
+      <section className={utilStyles.headingMdCenter}>
+        <br />
+        <h1 className={utilStyles.heading2Xl}>
+          E aí Galerinha, tudo beleza?!?!
+          <br />
+          Sejam todos bem vindos ao Canal DPRC!
+        </h1>
+        <br />
+        <p>
+          Esse é o site sobre canal DPRC no{" "}
+          <a href="/redir/mainchannel" target="_blank">
+            Youtube{" "}
+            <Image src="/images/yt_icon_rgb.png" height={20} width={26} />
+          </a>{" "}
+          protagonizado por Christian e MarceloP!!
         </p>
         <p>
-          Atualmente nosso canal está com {channelStatistics.subscriberCount} inscritos, e se você não é inscrito ainda, clique <a href="/redir/mainchannel?subscribe" target="_blank">aqui</a> e inscreva-se agora!!!
+          Atualmente nosso canal está com {channelStatistics.subscriberCount}{" "}
+          inscritos, e se você não é inscrito ainda, clique{" "}
+          <a href="/redir/mainchannel?subscribe" target="_blank">
+            aqui
+          </a>{" "}
+          e inscreva-se agora!!!
         </p>
-        <br/>
+        <br />
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <div className={utilStyles.headingLastPublish}>
-          <br/>
-          <h2 className={utilStyles.headingX2Center}>Vejam nossas últimas publicações</h2>
-          <br/>
+          <h2 className={utilStyles.headingX2Center}>
+            Vejam nossas últimas publicações
+          </h2>
         </div>
-        <br/>
+        <br />
         <div className={utilStyles.videoList}>
-          {lastestVideos.titles.map(({ id, title, description, pubDate, thumbnail }) => (
-            <>
-              <div className={utilStyles.boxes}>
-                <div className={utilStyles.thumb}>
-                <a href={`redir/${id}`} target="_blank"><Image src={thumbnail} width='120px' height="90px" /></a>
+          {lastestVideos.titles.map(
+            ({ id, title, description, pubDate, thumbnail }) => (
+              <>
+                <div className={utilStyles.boxes}>
+                  <div className={utilStyles.thumb}>
+                    <a href={`redir/${id}`} target="_blank">
+                      <Image src={thumbnail} width="120px" height="90px" />
+                    </a>
+                  </div>
+                  <div className={utilStyles.link}>
+                    <a href={`redir/${id}`} target="_blank">
+                      {title}
+                    </a>
+                  </div>
+                  <div className={utilStyles.videoDate}>
+                    <Date dateString={pubDate} />
+                  </div>
+                  <div className={utilStyles.desc}>{description}</div>
                 </div>
-                <div className={utilStyles.link}>
-                  <a href={`redir/${id}`} target="_blank">{title}</a>
-                </div>
-                <div className={utilStyles.videoDate}>
-                  <Date dateString={pubDate} />
-                </div>
-                <div className={utilStyles.desc}>
-                  {description}
-                </div>
-              </div>
-            </>
-          ))}
+              </>
+            )
+          )}
         </div>
       </section>
       <br />
@@ -73,38 +89,37 @@ export default function Home({ channelStatistics, lastestVideos }) {
         <div className={utilStyles.footerFlex}>
           <div>
             <a href="/redir/mainchannel?footer" target="_blank">
-              <Image
-                src="/images/yt_icon_rgb.png"
-                height={20}
-                width={26} />
+              <Image src="/images/yt_icon_rgb.png" height={20} width={26} />
               <br />
-          DPRC
-          </a>
+              DPRC
+            </a>
           </div>
           <div>
             <a href="/redir/twitter?footer" target="_blank">
               <Image
                 src="/images/twitter_icon_blue.png"
                 height={20}
-                width={20} />
+                width={20}
+              />
               <br />
-          @dprc_yt
-          </a>
+              @dprc_yt
+            </a>
           </div>
           <div>
             <a href="/redir/insta?footer" target="_blank">
               <Image
                 src="/images/instagram_icon_rgb.png"
                 height={20}
-                width={20} />
+                width={20}
+              />
               <br />
-          @dprc_yt
-          </a>
+              @dprc_yt
+            </a>
           </div>
         </div>
         <br />
         Canal DPRC - Todos os Direitos Reservados - 2021
       </footer>
     </Layout>
-  )
+  );
 }
